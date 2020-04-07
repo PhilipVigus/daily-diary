c9564b9e65f3e336c815b6b252d974c0521c8198
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-07 13:09:02 +0100

    Implement entry title list feature
    At present the feature is only implemented in its most basic form,
    with titles hard-coded into the class.
    
    The next step will be wiring up to a database and getting the
    titles from there instead.

--- daily_diary.rb
+++ daily_diary.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative './lib/entry'
 
 class DailyDiary < Sinatra::Base
   get '/' do
     erb :index
+  end
+
+  get '/entries' do
+    @titles = Entry.all_titles
+    erb :entries
   end
 
   run! if app_file == $0
