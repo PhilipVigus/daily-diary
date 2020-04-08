e2f6bf8d744b18e8c91d94eb5a9495c89d2c5f37
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-08 18:26:53 +0100

    Update all_titles to return instances of Entry

--- lib/entry.rb
+++ lib/entry.rb
@@ -2,9 +2,20 @@ require 'pg'
 require 'db_connection'
 
 class Entry
+  attr_reader :title, :body, :date, :id
+
+  def initialize(title, body, date, id = 0)
+    @title = title
+    @body = body
+    @date = date
+    @id = id
+  end
+
   def self.all_titles
     query_result = DBConnection.run_query('SELECT * FROM entries;')
-    query_result.map { |row| row['title'] }
+    query_result.map do |row|
+      Entry.new(row['title'], row['body'], row['date'], row['id'])
+    end
   end
 
   def self.create(title, body)
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -2,15 +2,34 @@ require 'entry'
 require 'db_connection'
 
 describe Entry do
-  #let(:conn) { PG.connect(dbname: 'daily_diary_test') }
+  subject do
+    Entry.new('Title', 'Body', '2020-04-08 18:15:25.71574')
+  end
 
   it 'gives you a list of all diary entries' do
-    DBConnection.run_query("INSERT INTO entries (body, title, date) VALUES ('This was a rubbish day', 'A rubbish day', NOW()), ('This was a better day', 'A better day', NOW());")
-    expect(Entry.all_titles).to eq(['A rubbish day', 'A better day'])
+    Entry.create('A rubbish day', 'This was a rubbish day')
+    Entry.create('A better day', 'This was a better day')
+    expect(Entry.all_titles[0].title).to eq('A rubbish day')
   end
 
   it 'allows you to create a new entry in the database' do
     Entry.create('An amazing day', 'Today was an amazing day')
     expect(Entry.all_titles).to include('An amazing day')
+  end
+
+  it 'has a title' do
+    expect(subject.title).to eq 'Title'
+  end
+
+  it 'has a body' do
+    expect(subject.body).to eq 'Body'
+  end
+
+  it 'has an id' do
+    expect(subject.id).to eq 0
+  end
+
+  it 'has a date' do
+    expect(subject.date).to eq '2020-04-08 18:15:25.71574'
   end
 end
