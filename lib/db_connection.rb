a28fac14acc42138505926bb36f71863c35af788
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-08 17:54:24 +0100

    Refactor DBConnection class
    The class was heavily refactored, making connect and disconnect
    private methods that are called by the run_query method. This means
    that all you need to do with the class is run_query and it will
    do the rest.
    
    There is an outstanding issue with this approach though. I cannot
    work out how to test for the case when connecting to the database
    fails. The way I've made the connect method private means that
    it is not visible to the RSpec allow_any_instance_of method, which
    is required because the DBConnection methods are class methods.

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -1,15 +1,6 @@
 class DBConnection
-  def self.connect
-    database = ENV['ENVIRONMENT'] == 'test' ? 'daily_diary_test' : 'daily_diary'
-    @conn = PG.connect(dbname: database)
-  end
-
-  def self.disconnect
-    @conn.close
-  end
-
   def self.run_query(query)
-    raise 'unable to run query - not connected to DB' if @conn.nil?
+    connect
     begin
       result = @conn.exec(query)
     ensure
@@ -19,7 +10,14 @@ class DBConnection
     result
   end
 
-  def self.conn
-    @conn
+  def self.connect
+    database = ENV['ENVIRONMENT'] == 'test' ? 'daily_diary_test' : 'daily_diary'
+    @conn = PG.connect(dbname: database)
   end
+
+  def self.disconnect
+    @conn.close
+  end
+
+  private_class_method :connect, :disconnect
 end
--- spec/db_connection_spec.rb
+++ spec/db_connection_spec.rb
@@ -1,39 +1,14 @@
 require 'db_connection'
 
 describe DBConnection do
-
-  describe '#connect' do
-    it 'connects to database' do
-      expect { DBConnection.connect }.not_to raise_error
-      DBConnection.disconnect
-    end
-  end
-
-  describe '#disconnect' do
-    it 'disconnects from an existing connection' do
-      DBConnection.connect
-      expect { DBConnection.disconnect }.not_to raise_error
-    end
-
-    it 'raises an error if you disconnect before connecting' do
-      expect { DBConnection.disconnect }.to raise_error PG::ConnectionBad
-    end
-  end
-
   describe '#run_query' do
     it 'runs properly formed SQL queries on the opened connection' do
-      DBConnection.connect
       expect(DBConnection.run_query('SELECT * from entries;').ntuples).to eq 0
     end
 
     context 'raising errors' do
       it 'raises an error if a SQL query is not properly formed' do
-        DBConnection.connect
         expect { DBConnection.run_query('SELECT * from wrong_table_name;')}.to raise_error PG::UndefinedTable
-      end
-
-      it 'raises an error if you try to run a query before you have connected' do
-        expect { DBConnection.run_query('SELECT * from entries;') }.to raise_error PG::ConnectionBad
       end
     end
   end
