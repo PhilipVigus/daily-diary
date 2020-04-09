a0905120e5f7b90a99049203b59b115f56262235
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-09 15:11:47 +0100

    Test and add find_by_id method to Entry class

--- lib/entry.rb
+++ lib/entry.rb
@@ -4,28 +4,27 @@ require_relative './db_connection'
 class Entry
   attr_reader :title, :body, :date, :id
 
-  def initialize(title, body, date = Date.now, id = 0)
+  def initialize(title, body, date = Date.now, id)
     @title = title
     @body = body
-    @date = date
-    @id = id
-  end
-
-  def self.all_titles
-    query_result = DBConnection.run_query('SELECT * FROM entries;')
-    query_result.map do |row|
-      Entry.new(row['title'], row['body'], row['date'], row['id'])
-    end
+    @date = DateTime.parse(date).to_date
+    @id = id.to_i
   end
 
   def self.all
-    query_result = DBConnection.run_query('SELECT * FROM entries;')
-    query_result.map do |row|
+    result = DBConnection.run_query('SELECT * FROM entries;')
+    result.map do |row|
       Entry.new(row['title'], row['body'], row['date'], row['id'])
     end
   end
 
   def self.create(title, body)
-    DBConnection.run_query("INSERT INTO entries (title, body, date) VALUES ($$#{title}$$, $$#{body}$$, NOW());")
+    result = DBConnection.run_query("INSERT INTO entries (title, body, date) VALUES ($$#{title}$$, $$#{body}$$, NOW()) RETURNING id, title, body, date;")
+    Entry.new(result[0]['title'], result[0]['body'], result[0]['date'], result[0]['id'])
+  end
+
+  def self.find_by_id(id)
+    result = DBConnection.run_query("SELECT * FROM entries WHERE id=#{id};")
+    Entry.new(result[0]['title'], result[0]['body'], result[0]['date'], result[0]['id'])
   end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -2,47 +2,38 @@ require 'entry'
 require 'db_connection'
 
 describe Entry do
-  subject do
-    Entry.new('Title', 'Body', '2020-04-08 18:15:25.71574')
-  end
-
-  describe '#all' do
+  describe '.all' do
     it 'gives you a list of all diary entries with the first entry present' do
-      Entry.create('A rubbish day', 'This was a rubbish day')
+      entry = Entry.create('A rubbish day', 'This was a rubbish day')
       Entry.create('A better day', 'This was a better day')
-      expect(Entry.all[0].title).to eq('A rubbish day')
-    end
-
-    it 'gives you a list of all diary entries with the second present' do
-      Entry.create('A rubbish day', 'This was a rubbish day')
-      Entry.create('A better day', 'This was a better day')
-      expect(Entry.all[1].title).to eq('A better day')
+      entries = Entry.all
+      expect(entries.length).to eq 2
+      expect(entries.first).to be_a Entry
+      expect(entries.first.title).to eq 'A rubbish day'
+      expect(entries.first.body).to eq 'This was a rubbish day'
+      expect(entries.first.id).to eq entry.id
     end
   end
 
-  describe '#create' do
+  describe '.create' do
     it 'creates a new entry in the database with the correct title' do
-      Entry.create('An amazing day', 'Today was an amazing day')
-      result = DBConnection.run_query('SELECT * FROM entries;')
-      expect(result.ntuples).to eq 1
+      entry = Entry.create('An amazing day', 'Today was an amazing day')
+      entry_in_db = DBConnection.run_query("SELECT * FROM entries WHERE id=#{entry.id};")
+      expect(entry.title).to eq 'An amazing day'
+      expect(entry.body).to eq 'Today was an amazing day'
+      expect(entry.date).to be_a Date
+      expect(entry.id).to eq entry_in_db.first['id'].to_i
     end
   end
 
-  describe 'instance getters' do
-    it 'has a title' do
-      expect(subject.title).to eq 'Title'
-    end
-
-    it 'has a body' do
-      expect(subject.body).to eq 'Body'
-    end
-
-    it 'has an id' do
-      expect(subject.id).to eq 0
-    end
-
-    it 'has a date' do
-      expect(subject.date).to eq '2020-04-08 18:15:25.71574'
+  describe '.find_by_id' do
+    it 'returns the entry with the specified id' do
+      created_entry = Entry.create('A rubbish day', 'This was a rubbish day')
+      returned_entry = Entry.find_by_id(created_entry.id)
+      expect(returned_entry.id).to eq created_entry.id
+      expect(returned_entry.title).to eq created_entry.title
+      expect(returned_entry.body).to eq created_entry.body
+      expect(returned_entry.date).to eq created_entry.date
     end
   end
 end
